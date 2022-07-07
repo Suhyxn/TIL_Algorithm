d7d7c51f2a5ae12c033571740d6a7e10611b5c15
Suhyxn <momentomori21489@gmail.com>
2022-07-07 21:51:52 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let a = Number(input[0].split(' ')[0]);
+let b = Number(input[0].split(' ')[1]);
+let c = Number(input[0].split(' ')[2]);
+
+console.log(`         ,r'"7
+r\`-_   ,'  ,/
+ \\. ". L_r'
+   \`~\\/
+      |
+      |`);
